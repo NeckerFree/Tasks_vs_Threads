3e2d8c6b3c676cc1800ee587312c34847984d6e4
Elio Cortes <elio.cortes3000@gmail.com>
2023-03-07 23:29:36 -0500

    added Projects Facade and Tests

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -8,12 +8,24 @@
 //Console.WriteLine(exp1);
 
 // Thread Pool
-double exp1 = 0;
-var mres = new ManualResetEventSlim(false);
-ThreadPool.QueueUserWorkItem<ManualResetEventSlim>((_mres) =>
-{
-    exp1 = Math.Exp(40);
-    mres.Set();
-}, mres, false);
-// ...
-mres.Wait();
+//double exp1 = 0;
+//var mres = new ManualResetEventSlim(false);
+//ThreadPool.QueueUserWorkItem<ManualResetEventSlim>((_mres) =>
+//{
+//    exp1 = Math.Exp(40);
+//    mres.Set();
+//}, mres, false);
+//// ...
+//mres.Wait();
+//Console.WriteLine("Task Started");
+//var task = Task.Run(() => Math.Exp(40));
+//var taskResult = task.Result;
+//Console.WriteLine(taskResult);
+
+
+using TaskVsThread;
+
+Body body = null!;
+CarBuildingWithTask carBuildingWithTask =new CarBuildingWithTask();
+var bodyThread = new Thread(() => {body= CarBuildingWithTask.BuildBody(100, 5, 2); });
+bodyThread.Start();
